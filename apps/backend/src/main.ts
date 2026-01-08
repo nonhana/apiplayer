8d460eca97c4fe65ab0a4548ae796eeef95d7026
non_hana <nonhana@outlook.com>
2026-01-08 11:31:46 +0800

    chore: backend add prod cor

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -66,7 +66,7 @@ async function bootstrap() {
 
   if (nodeEnv !== 'production') {
     app.enableCors({
-      origin: ['http://localhost:5173', 'http://localhost:3000'],
+      origin: ['http://localhost:5173', 'https://apiplayer.caelum.moe'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
